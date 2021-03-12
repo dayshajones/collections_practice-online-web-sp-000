2db9040e81768e2981a50494216136fceef284de
Daysha Jones <daysha.vu@icloud.com>
2021-03-12 22:27:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def kesha_maker(array)
+    array.each {|element| element[2] = "$"}
+end
 
+def find_a(array)
+    array.select{|element| element[0] == "a"}
+end
+
+def sum_array(array)
+    array.inject{|add, num| add+num}
+end
+
+def add_s(array)
+    array.each_with_index.collect{|element, index|  
+    if index == 1
+     element = element
+    else
+    element = element + "s"
+  end
+  }
+end
